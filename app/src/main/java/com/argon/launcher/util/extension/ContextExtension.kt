2a1bf4f673da5e5ff9373a04ceaf28c1c5b19dd8
amkrys <amkrys007@gmail.com>
2024-06-29 16:01:54 +0530

    Added loge extension function

--- app/src/main/java/com/argon/launcher/util/extension/ContextExtension.kt
+++ app/src/main/java/com/argon/launcher/util/extension/ContextExtension.kt
@@ -1,9 +1,10 @@
 package com.argon.launcher.util.extension
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.datastore.preferences.preferencesDataStore
+import com.argon.launcher.BuildConfig
 import com.argon.launcher.util.constant.Constants
 import java.io.File
 
@@ -18,5 +19,9 @@ fun Context.getIconFolderPath(label: String): String {
 
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.log(message: String?) {
+    if (BuildConfig.DEBUG) message?.let { Log.e(this::class.java.simpleName, it) }
 }
 
